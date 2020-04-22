c61b0405bf074f508ce0134ba17ce27e78225882
VaskoBoshkov <48734543+VaskoBoshkov@users.noreply.github.com>
2020-04-22 16:33:22 +0200

    Updates

---
+++ precache-manifest.d5d30c531bc88dbd4d1d951270d81cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fd721638003a853bdb81d627e811bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b755ac0f004ddf4353f",
+    "url": "/monsters-rolodex/static/css/main.13c23824.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b755ac0f004ddf4353f",
+    "url": "/monsters-rolodex/static/js/main.4c6493e0.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
